19d58a1407c9088807e132d644b817dd24db7d13
Bruno Batista <brunobatista101@gmail.com>
2018-01-29 10:19:47 -0200

    config yml

--- tests/spec/spec_helper.rb
+++ tests/spec/spec_helper.rb
@@ -13,7 +13,8 @@ require 'ostruct'
 require 'yaml'
 require_relative '../suport/page_helper.rb'
 
-env_yaml = YAML.load_file("#{Dir.pwd}/suport/rspec.yml")
+rspec_yml = YAML.load_file("#{Dir.pwd}/suport/rspec.yml")
+env = YAML.load_file("#{Dir.pwd}/suport/#{rspec_yml['environment']}.yml")
 
 RSpec.configure do |config|
   config.before(:each) do
@@ -33,8 +34,8 @@ end
 # Escolhendo qual browser se quero chrome ou se quero firefox
 # E se roda em headless ou sem headless
 Capybara.register_driver :selenium do |app|
-  if env_yaml['browser'].eql?('chrome')
-    if env_yaml['headless'].eql?('headless')
+  if rspec_yml['browser'].eql?('chrome')
+    if rspec_yml['headless'].eql?('headless')
       preferences = { credentials_enable_service: false,
                       password_manager_enabled: false }
       Capybara::Selenium::Driver.new(
@@ -47,7 +48,7 @@ Capybara.register_driver :selenium do |app|
                                'prefs' => preferences }
         )
       )
-    elsif env_yaml['headless'].eql?('no_headless')
+    elsif rspec_yml['headless'].eql?('no_headless')
       preferences = { credentials_enable_service: false,
                       password_manager_enabled: false }
       Capybara::Selenium::Driver.new(
@@ -60,8 +61,8 @@ Capybara.register_driver :selenium do |app|
         )
       )
     end
-  elsif env_yaml['browser'].eql?('firefox')
-    if env_yaml['headless'].eql?('headless')
+  elsif rspec_yml['browser'].eql?('firefox')
+    if rspec_yml['headless'].eql?('headless')
       browser_options = Selenium::WebDriver::Firefox::Options.new(args: ['--headless'])
       Capybara::Selenium::Driver.new(
         app,
@@ -69,14 +70,14 @@ Capybara.register_driver :selenium do |app|
         options: browser_options
       )
     end
-  elsif env_yaml['headless'].eql?('no_headless')
+  elsif rspec_yml['headless'].eql?('no_headless')
     Capybara::Selenium::Driver.new(app, browser: :firefox, marionette: true)
   end
 end
 
 Capybara.configure do |config|
   config.default_driver = :selenium
-  config.app_host = 'http://demo.automationtesting.in/'
+  config.app_host = env['url']
   config.default_max_wait_time = 10
 end
 
